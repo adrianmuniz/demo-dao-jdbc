907eed692ebab9fabbe28e27d3052208379f36fe
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-06-05 19:56:41 -0300

    Implementation update

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,10 @@ public class Program {
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! new id = " + newSeller.getId());
 		
-		
+		System.out.println("\n=== TESTE 5: Seller Update ====");
+		seller = sellerDao.findById(1);
+		seller.setName("Marta Wayne");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 }
