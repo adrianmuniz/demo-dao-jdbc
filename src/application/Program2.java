bb80b7101cba0e4873db3abaea339a00da8baf57
Cainã Adrian <adrianmuniz.ceo@outlook.com>
2022-06-08 23:14:43 -0300

    findAll implementation

--- src/application/Program2.java
+++ src/application/Program2.java
@@ -1,9 +1,11 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import model.dao.DaoFactory;
 import model.dao.DepartmentDao;
 import model.entities.Department;
-import model.entities.Seller;
 
 public class Program2 {
 
@@ -15,16 +17,26 @@ public class Program2 {
 		Department newDepartment = new Department(null, "Music");
 		departmentDao.insert(newDepartment);
 		System.out.println("Inserted! new department: " + newDepartment.getId() + " - " + newDepartment.getName());
+		System.out.println();
 		
 		System.out.println("==== TESTE 2 : Department update ====");
 		newDepartment = departmentDao.findById(5);
 		newDepartment.setName("Adm");
 		departmentDao.update(newDepartment);
 		System.out.println("Update completed");
+		System.out.println();
 		
 		System.out.println("=== TESTE 3: Department findById ====");
 		Department dep = departmentDao.findById(3);
 		System.out.println(dep);
+		System.out.println();
+		
+		System.out.println("=== TESTE 4: Department findAll ====");
+		List<Department> list = new ArrayList<>();
+		list = departmentDao.findAll();
+		for (Department obj : list) {
+			System.out.println(obj);
+		}
 
 	}
 
